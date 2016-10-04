c08cecebe8556d1dcab3ca81e8c672c39fc4a94d
Ubuntu <ubuntu@ip-172-31-45-181.us-west-2.compute.internal>
2016-10-04 01:59:46 +0000

    part 1b

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var http = require('http');
 var port = Number(process.env.PORT || 8080);
 var fs = require('fs');
-var data = fs.readFileSync("./public/index.html");
+//var data = fs.readFileSync("./public/index.html");
 
 var requestListener = function(req,res){
 	res.writeHead(200,{"Content-type":"text/html"});
-	res.end(data);
-}
+	//res.end(data);
+	var data = fs.readFile("./public/index.html", function(err, data){
+		if (err){ 
+			throw err;
+		}
+		res.end(data);
+	})
+};
+
 var server = http.createServer(requestListener);
 server.listen(port,function(){
 	console.log("node app is running at port:", port);
